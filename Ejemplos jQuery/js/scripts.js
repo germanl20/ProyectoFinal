d5f5bcf7908bbaf25707199df0565b8b3266f4a5
Germanl20 <germanlopezz16@hotmail.com>
2019-03-06 01:08:25 -0300

    Se agregaron ejemplos de jquery

--- Ejemplos jQuery/js/scripts.js
+++ Ejemplos jQuery/js/scripts.js
@@ -11,4 +11,9 @@ $(function(){
     $('main article:first'); // SELECCIONO EL PRIMER ARTICLE DEL ARRAY
     $('main article:last'); // SELECCIONO EL ULTIMO ARTICLE DEL ARRAY
     $('main article:last').hide(); // OCULTO COMO SI FUERA DISPLAY = "NONE"
+    $('main article:first').remove(); // BORRO EL ELEMENTO DEL DOM
+
+    var copia = $('main article:last').clone(); // CLONO O DUPLICO UN ELEMENTO
+    $('main').append(copia); // ES COMO EL APPENDCHILD DE JAVASCRIPT, AGREGO AL FINAL DEL MAIN EL ELEMENTO ARTICLE
+    $('main').prepend(copia); // AGREGO AL INICIO DEL MAIN EL ELEMENTO
 });
